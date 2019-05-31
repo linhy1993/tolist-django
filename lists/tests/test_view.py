ed83e609470944096aaa4505922ea2961190e633
linhy1993 <linhongyi1110@gmail.com>
2019-05-31 12:49:44 -0400

    Split out unit test into two files

--- lists/tests/test_view.py
+++ lists/tests/test_view.py
@@ -3,25 +3,6 @@ from django.test import TestCase
 from lists.models import Item, List
 
 
-class HomePageTest(TestCase):
-
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'home.html')
-
-    # def test_can_save_a_POST_request(self):
-    #     response = self.client.post('/', data={'item_text': 'A new list item'})
-    #
-    #     self.assertEqual(Item.objects.count(), 1)
-    #     new_item = Item.objects.first()
-    #     self.assertEqual(new_item.text, 'A new list item')
-    #
-    # def test_redirects_after_POST(self):
-    #     response = self.client.post('/', data={'item_text': 'A new list item'})
-    #     self.assertEqual(response.status_code, 302)
-    #     self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-
 class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
