f293ea6169546b209200d43c9bcfad2f65a8f12c
linhy1993 <linhongyi1110@gmail.com>
2019-06-26 11:21:13 -0400

    add test login

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -62,9 +62,9 @@ class FunctionalTest(StaticLiveServerTestCase):
             # firefox_profile=profile,
             # capabilities=cap,
             log_path=op.join(basedir, 'geckodriver.log'))
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
+        self.staging_server = os.environ.get('STAGING_SERVER')
+        if self.staging_server:
+            self.live_server_url = 'http://' + self.staging_server
 
     def tearDown(self):
         self.browser.quit()
